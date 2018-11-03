6ee6584743fc10b3feae20adfe61271714f6ba19
shrek1402 <44646716+shrek1402@users.noreply.github.com>
2018-11-03 17:26:08 +0700

    add lab7

--- lab7.c
+++ lab7.c
@@ -1,114 +1,70 @@
-#include <locale.h>
-#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
-int ntrl(int num)
-{
-    if (num > 9 && num < 100)
-    {
-        int j;
-        //yyyyy
-//kgjfgfdgfdhfdsfdsgfdsfdsfdsfdsfdsfdsgfdsgfdsfd
-        for (j = 2; j <= sqrt(num); j++)
-        {
-            if (num % j == 0)
-                return (1); // -1 prostoe
-        }
-
-        if (num % j != 0 && num != j)
-            return (-1);
-    }
-    else
-        return (0);
-}
-
-float arithmetick_mean(int *arr, int lot)
-{
-    float m, sum = 0, k = 0;
-
-    for (int j = 1; j <= lot; j++)
-    {
-        sum = sum + arr[j];
-        k++;
-    }
-
-    m = (sum / k);
-
-    if (k)
-        return (m);
-    else
-        return (-1);
-}
-
-int maxs(int *arr, int size)
-{
-    int j, max = arr[1];
-
-    for (j = 1; j <= size; j++)
-        if (arr[j] > max)
-            max = arr[j];
-
-    return (max);
-}
-
-void remove_last_item(int **array, int *size)
-{
-    (*size)--;
-    *array = (int *)realloc(*array, *size * sizeof(int));
-    if (!(*array))
-        printf("Ошибка\n");
-}
 
 int main()
 {
-    int *a=NULL;
-    int i, j, k = 0, m, n, max_1, max_2;
-    float arithmetick_mean_1, arithmetick_mean_2;
- 
     srand(time(NULL));
-    scanf("%d", &n);
-    printf("# Знач. (-1,0,1) \n");
-    a = (int *)malloc(n * sizeof(int));
+    int  **a = NULL; // Матрица 
+    float *D = NULL; // Вектор среднего ариф. каждой строки
+    int   *G = NULL; // вектор кол-ва отриц. эл. каждого столбца
+    int sum = 0, i, j, str, stlb, kol_otr = 0;
 
-    for (i = 1; i <= n; i++)
+    printf("Enter size array\nStrok: ");
+    scanf("%d", &str);
+
+    printf("Stolbcov: ");
+    scanf("%d", &stlb);
+
+    a = (int **)malloc(str * sizeof(int*));
+
+    for (i = 0; i < str; i++)
     {
-        a[i] = rand() % 100 + 1;
-        printf("%-2d%6d%10d\n", i, a[i], ntrl(a[i]));
+        a[i] = (int *)malloc(stlb * sizeof(int));
     }
 
-    max_1 = maxs(a, n);
-    arithmetick_mean_1 = arithmetick_mean(a, n);
-
-    printf("Максимальное значение = %d\n", max_1);
-    printf("Среднее арифметическое = %.1f\n\n\n\n", arithmetick_mean_1);
-
-    for (i = 1; i <= n; i++)
+    for (i = 0; i < str; i++)
     {
-        if (ntrl(a[i]) == -1)
+        for (j = 0; j < stlb; j++)
         {
-            for (j = i; j < n; j++)
-                a[j] = a[j + 1];
-            i--;
-            remove_last_item(&a, &n);
+            a[i][j] = rand() % 21 - 10;
         }
     }
+    for (i = 0; i < str; i++)
+    {
+        for (j = 0; j < stlb; j++)
+            printf("%4d", a[i][j]);
 
-    printf("# Знач. (0,1) \n");
+        printf("\n");
+    }
 
-    for (i = 1; i <= n; i++)
-        printf("%-2d%6d%10d\n", i, a[i], ntrl(a[i]));
+    printf("\n");
 
-    max_2 = maxs(a, n);
-    arithmetick_mean_2 = arithmetick_mean(a, n);
+    D = (float *)malloc(str * sizeof(float));
 
-    printf("Максимальное значение = %d\n", max_2);
-    printf("Среднее арифметическое = %.1f\n\n\n\n", arithmetick_mean_2);
+    for (i = 0; i < str; i++)
+    {
+        sum = 0;
+        for (j = 0; j < stlb; j++)
+        {
+            sum += a[i][j];
+        }
 
-    max_1 == max_2 ? printf("Максимальное значение осталось прежним\n") : printf("Максимальное значение изменилось\n");
+        D[i] = ((float)sum / (float)stlb);
+        printf("|%4.1f|\n", D[i]);
+    }
 
-    arithmetick_mean_1 == arithmetick_mean_2 ? printf("Среднее значение осталось прежним\n") : printf("Среднее значение изменилось\n");
-    free(a);
-    system("PAUSE");
+    G = (int*) malloc (stlb * sizeof (int));
+    printf("|");
+    for (i=0; i<str; i++)
+    {
+        kol_otr = 0;
+        for (j=0; j<stlb; j++)
+            if (a[j][i] < 0)
+            kol_otr +=1;
+  G[i] = kol_otr;
+    printf("%2d", G[i]);
+    
+    }
+    printf("|");
 }
