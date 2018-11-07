3525fcfb71b15a46652a4079847e22990f5a9a19
shrek1402 <44646716+shrek1402@users.noreply.github.com>
2018-11-08 02:22:01 +0700

    add polindrom for big num

---
+++ polindrom.c
@@ -0,0 +1,36 @@
+#include <stdio.h> //53.9
+#include <stdlib.h>
+
+unsigned long long int perevorot(unsigned long long int y)
+{
+    unsigned long long int m = 0, z = y;
+    while (z > 0)
+    {
+        m = m * 10 + (z) % 10;
+        (z) = (z) / 10;
+    }
+    return (m);
+}
+
+void polindrom(unsigned long long int *x)
+{
+    int i = 0;
+    while ((*x) != perevorot(*x))
+    {
+        i++;
+        printf("i= %llu\n", i);
+        printf("x= %llu\n", *x);
+        printf("p= %llu\n", perevorot(*x));
+        (*x) += perevorot(*x);
+    }
+    printf("i = %llu\n", i);
+}
+
+int main()
+{
+    int m = 0;
+    unsigned long long int num;
+    scanf("%llu", &num);
+    polindrom(&num);
+    printf("***%llu***", num);
+}
