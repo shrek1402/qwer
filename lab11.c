d44cf5e4eb611968866ec6495a8c5f0ce5999386
shrek1402 <44646716+shrek1402@users.noreply.github.com>
2018-11-16 00:13:55 +0700

    5

---
+++ lab11.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include <time.h>
+#include<stdlib.h>
+int kr_5(int **array)
+{
+    int i, j,k=0;
+
+    for (i = 0; i < 5; i++)
+        for (j = 0; j < 5; j++)
+            if (array[i][j] % 5 == 0)
+                k++;
+    return (k);
+}
+
+int main()
+{
+    int **a=NULL,i,j;
+
+    a = (int **)malloc(5 * sizeof(int *));
+    if (!(a))
+    {
+        printf("Error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Создание массива указателей столбцов
+    for (i = 0; i < 5; i++)
+    {
+        a[i] = (int *)malloc(5 * sizeof(int));
+        if (!(a[i]))
+        {
+            printf("Error!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            a[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%2d", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Kol-vo elementov kratnih 5: %d\n", kr_5(a));
+}
